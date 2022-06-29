eb0f2e216e247666ebf0c379300379dd30ca9551
Josias Santos <josiassantos1577@gmail.com>
2022-06-28 23:28:00 -0300

    segundo commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,61 @@
 // JSX = Javascript + xml
 import { Header } from './components/Header'
-import { Post } from './Post';
+import { Post } from './components/Post';
+import { Sidebar } from './components/Sidebar'
 
 import styles from './App.module.css';
+
 import './global.css';
 
 export function App() {
+
+  const posts = [
+    {
+      id: 1,
+      author: {
+        avatarUrl: "https://github.com/josiassantos15.png",
+        name: 'Josias Santos',
+        role: 'Programador'
+      },
+      content: [
+        { type: 'paragraph', content: 'Fala galeraa 👋', },
+        { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+        { type: 'link', content: 'jane.design/doctorcare' },
+      ],
+      publishedAt: new Date('2022-06-27 20:00:00'),
+    },
+    {
+      id: 2,
+      author: {
+        avatarUrl: "https://github.com/diego3g.png",
+        name: 'Diego Fernandes',
+        role: 'Programador'
+      },
+      content: [
+        { type: 'paragraph', content: 'Fala galeraa 👋', },
+        { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+        { type: 'link', content: 'jane.design/doctorcare' },
+      ],
+      publishedAt: new Date('2022-06-28 22:00:00'),
+    },
+  ]
+
   return (
     <div>
       <Header />
       <div className={styles.wrapper}>
         <Sidebar />
           <main>
-            <Post 
-              author="Josias Santos" 
-              content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, magni quasi possimus sed esse dolorum explicabo laudantium impedit autem amet nemo dolores aut. Assumenda atque voluptate nihil, necessitatibus odio at."  
-            />
+           {posts.map(post => {
+              return (
+                <Post 
+                  key={post.id}
+                  author={post.author}
+                  content={post.content}
+                  publishedAt={post.publishedAt}
+                />
+              )
+           })}
           </main>
         </div>
     </div>
